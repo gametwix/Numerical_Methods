5f885aa16c85f988c64ff07c786b14b8ef7742ee
Pavel Mokhliakov <pmokhliakov@gmail.com>
2022-10-10 13:03:40 +0300

    all to matrix solve

--- Lab5/LB51/PDE.py
+++ Lab5/LB51/PDE.py
@@ -1,14 +1,21 @@
 import numpy as np
-from math import sin, cos, exp
+import scipy
+from numba import jit
+
+
 
 class PartialDifferentialEquation:
-    def __init__(self, a=1, b=0, c=0,
-                 Ut_start_x=lambda t, a, b, c: 0,
-                 Ut_finish_x=lambda t, a, b, c: 0,
-                 Ux_start_t=lambda x, a, b, c: 0,
-                 U_answer=lambda x, t, a, b, c: 0,
-                 fi=lambda x, t, a, b, c: 0,
-                 ):
+    def __init__(
+        self,
+        a=1,
+        b=0,
+        c=0,
+        Ut_start_x=lambda t, a, b, c: 0,
+        Ut_finish_x=lambda t, a, b, c: 0,
+        Ux_start_t=lambda x, a, b, c: 0,
+        U_answer=lambda x, t, a, b, c: 0,
+        fi=lambda x, t, a, b, c: 0,
+    ):
         self.a = a
         self.b = b
         self.c = c
@@ -34,114 +41,117 @@ class PartialDifferentialEquation:
         return self._f_fi(x, t, a=self.a, b=self.b, c=self.c)
 
 
-
-def Explicit_method(Equation, start_x, finish_x,
-                    resolution_x, finish_t,
+def Explicit_method(Equation,
+                    start_x,
+                    finish_x,
+                    resolution_x,
+                    finish_t,
                     resolution_t
                     ):
     h = (finish_x - start_x) / (resolution_x - 1)
     tau = finish_t / (resolution_t - 1)
-    x = np.array([start_x + h*i for i in range(resolution_x)])
+    x = np.array([start_x + h * i for i in range(resolution_x)])
     answer = np.zeros((resolution_t, resolution_x))
 
-    cof_a = Equation.a*tau / h**2
-    cof_b = Equation.b*tau / 2*h
-    cof_c = Equation.c*tau
+    cof_a = Equation.a * tau / h**2
+    cof_b = Equation.b * tau / 2 * h
+    cof_c = Equation.c * tau
 
     answer[0, :] = [Equation.Ux_start_t(elem) for elem in x]
     for k in range(resolution_t - 1):
-        answer[k+1][0] = Equation.Ut_start_x(k*tau)
-        answer[k+1][resolution_x - 1] = Equation.Ut_finish_x(k*tau)
+        answer[k + 1][0] = Equation.Ut_start_x((k+1) * tau)
+        answer[k + 1][resolution_x - 1] = Equation.Ut_finish_x((k+1) * tau)
         for i in range(1, resolution_x - 1):
-            answer[k+1][i] += (cof_c + 1 - 2*cof_a) * answer[k][i]
-            answer[k+1][i] += (cof_a - cof_b) * answer[k][i-1]
-            answer[k+1][i] += (cof_a + cof_b) * answer[k][i+1]
-            answer[k+1][i] += Equation.fi(i*h + start_x, tau*k)
+            answer[k + 1][i] += ((cof_c + 1 - 2 * cof_a) * answer[k][i]
+                                 + (cof_a - cof_b) * answer[k][i - 1]
+                                 + (cof_a + cof_b) * answer[k][i + 1]
+                                 + Equation.fi(i * h + start_x, tau * k))
     return answer
 
 
-def Implicit_method(Equation, start_x, finish_x,
-                    resolution_x, finish_t,
+def Implicit_method(Equation,
+                    start_x,
+                    finish_x,
+                    resolution_x,
+                    finish_t,
                     resolution_t
                     ):
     h = (finish_x - start_x) / (resolution_x - 1)
     tau = finish_t / (resolution_t - 1)
-    x = np.array([start_x + h*i for i in range(resolution_x)])
+    x = np.array([start_x + h * i for i in range(resolution_x)])
     answer = np.zeros((resolution_t, resolution_x))
 
-    cof_a = Equation.a*tau / h**2
-    cof_b = Equation.b*tau / 2*h
-    cof_c = Equation.c*tau
+    cof_a = Equation.a * tau / h**2
+    cof_b = Equation.b * tau / 2 * h
+    cof_c = Equation.c * tau
 
     answer[0, :] = [Equation.Ux_start_t(elem) for elem in x]
     for k in range(resolution_t - 1):
-        answer[k+1][0] = Equation.Ut_start_x(k*tau)
-        answer[k+1][resolution_x - 1] = Equation.Ut_finish_x(k*tau)
-        new_line_mat = np.zeros((resolution_x-2, resolution_x-2))
-        new_line_vec = np.zeros(resolution_x-2)
-        for i in range(resolution_x-2):
-            new_line_mat[i][i] = (2*cof_a + cof_c + 1)
-            new_line_vec[i] = answer[k][i+1] + \
-                Equation.fi((i+1) * h + start_x, tau * (k+1))
+        new_line_mat = np.zeros((resolution_x, resolution_x))
+        new_line_vec = np.zeros(resolution_x)
+        new_line_mat[0][0] = 1
+        new_line_mat[resolution_x - 1][resolution_x - 1] = 1
+        new_line_vec[0] = Equation.Ut_start_x(k * tau)
+        new_line_vec[resolution_x - 1] = Equation.Ut_finish_x(k * tau)
+        for i in range(1, resolution_x - 1):
+            new_line_mat[i][i - 1] = cof_b - cof_a
+            new_line_mat[i][i] = 2 * cof_a + cof_c + 1
+            new_line_mat[i][i + 1] = -(cof_b + cof_a)
 
-            if i == 0:
-                new_line_mat[i][i+1] = -(cof_b + cof_a)
-                new_line_vec[i] += (cof_a - cof_b)*answer[k+1][0]
-            elif i == resolution_x-3:
-                new_line_mat[i][i-1] = cof_b - cof_a
-                new_line_vec[i] += (cof_a + cof_b)*answer[k+1][resolution_x - 1]
-            else:
-                new_line_mat[i][i+1] = -(cof_b + cof_a)
-                new_line_mat[i][i-1] = cof_b - cof_a
-
-        line = np.linalg.solve(new_line_mat, new_line_vec)
-        answer[k+1][1:-1] = line
+            new_line_vec[i] = answer[k][i] + Equation.fi(
+                i * h + start_x, tau * (k + 1)
+            )
+        line = scipy.linalg.solve(new_line_mat, new_line_vec)
+        answer[k + 1] = line
     return answer
 
 
-def Explicit_Implicit_method(Equation, start_x, finish_x,
-                             resolution_x, finish_t,
-                             resolution_t, teta=0.5
-                             ):
+def Explicit_Implicit_method(Equation,
+                             start_x,
+                             finish_x,
+                             resolution_x,
+                             finish_t,
+                             resolution_t,
+                             teta=0.5):
     h = (finish_x - start_x) / (resolution_x - 1)
     tau = finish_t / (resolution_t - 1)
-    x = np.array([start_x + h*i for i in range(resolution_x)])
+    x = np.array([start_x + h * i for i in range(resolution_x)])
     answer = np.zeros((resolution_t, resolution_x))
 
-    cof_a_imp = Equation.a*tau*teta / h**2
-    cof_b_imp = Equation.b*tau*teta / 2*h
-    cof_c_imp = Equation.c*tau*teta
-    cof_a_exp = Equation.a*tau*(1-teta) / h**2
-    cof_b_exp = Equation.b*tau*(1-teta) / 2*h
-    cof_c_exp = Equation.c*tau*(1-teta)
+    #  From Implicit
+    cof_a_imp = Equation.a * tau * teta / h**2
+    cof_b_imp = Equation.b * tau * teta / 2 * h
+    cof_c_imp = Equation.c * tau * teta
+    #  From Explicit
+    cof_a_exp = Equation.a * tau * (1 - teta) / h**2
+    cof_b_exp = Equation.b * tau * (1 - teta) / 2 * h
+    cof_c_exp = Equation.c * tau * (1 - teta)
 
     answer[0, :] = [Equation.Ux_start_t(elem) for elem in x]
     for k in range(resolution_t - 1):
-        answer[k+1][0] = Equation.Ut_start_x(k*tau)
-        answer[k+1][resolution_x - 1] = Equation.Ut_finish_x(k*tau)
-        new_line_mat = np.zeros((resolution_x-2, resolution_x-2))
-        new_line_vec = np.zeros(resolution_x-2)
-        for i in range(resolution_x-2):
-            new_line_mat[i][i] = (2*cof_a_imp + cof_c_imp + 1)
-            new_line_vec[i] = teta*Equation.fi((i+1) * h + start_x, tau * (k+1))\
-                + (cof_c_exp + 1 - 2*cof_a_exp) * answer[k][i+1]\
-                + (cof_a_exp - cof_b_exp) * answer[k][i]\
-                + (cof_a_exp + cof_b_exp) * answer[k][i+2]\
-                + (1 - teta)*Equation.fi((i+1) * h + start_x, tau * k)
-
-            if i == 0:
-                new_line_mat[i][i+1] = -(cof_b_imp+cof_a_imp)
-                new_line_vec[i] += (cof_a_imp-cof_b_imp) * answer[k+1][0]
-            elif i == resolution_x-3:
-                new_line_mat[i][i-1] = cof_b_imp - cof_a_imp
-                new_line_vec[i] += (cof_a_imp+cof_b_imp) * answer[k+1][resolution_x - 1]
-            else:
-                new_line_mat[i][i+1] = -(cof_b_imp + cof_a_imp)
-                new_line_mat[i][i-1] = cof_b_imp - cof_a_imp
-
+        #  From Implicit
+        new_line_mat = np.zeros((resolution_x, resolution_x))
+        new_line_vec = np.zeros(resolution_x)
+        new_line_mat[0][0] = 1
+        new_line_mat[resolution_x - 1][resolution_x - 1] = 1
+        new_line_vec[0] = Equation.Ut_start_x(k * tau)
+        new_line_vec[resolution_x - 1] = Equation.Ut_finish_x(k * tau)
+        
+        for i in range(1, resolution_x - 1):
+            #  From Implicit
+            new_line_mat[i][i - 1] = cof_b_imp - cof_a_imp
+            new_line_mat[i][i] = 2 * cof_a_imp + cof_c_imp + 1
+            new_line_mat[i][i + 1] = -(cof_b_imp + cof_a_imp)
+            
+            new_line_vec[i] = (
+                #  From Implicit
+                teta * Equation.fi(i * h + start_x, tau * (k + 1))
+                #  From Explicit
+                + (cof_c_exp + 1 - 2 * cof_a_exp) * answer[k][i]
+                + (cof_a_exp - cof_b_exp) * answer[k][i - 1]
+                + (cof_a_exp + cof_b_exp) * answer[k][i + 1]
+                + (1 - teta) * Equation.fi(i * h + start_x, tau * k)
+            )
         line = np.linalg.solve(new_line_mat, new_line_vec)
-        answer[k+1][1:-1] = line
+        answer[k + 1] = line
     return answer
-
-
-    
--- Lab5/LB51/main.py
+++ Lab5/LB51/main.py
@@ -1,8 +1,7 @@
-from cProfile import label
 from math import sin, cos, exp
 
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, TextBox 
+from matplotlib.widgets import Slider, TextBox
 import numpy as np
 
 import PDE
@@ -10,36 +9,43 @@ import PDE
 
 def get_func_x(str_math_func: str):
     code = compile(str_math_func, "<string>", "eval")
+
     def func(x, **kwargs):
-        kwargs['x'] = x
-        return eval(code, {'sin': sin, 'cos': cos, 'exp': exp}, kwargs)
+        kwargs["x"] = x
+        return eval(code, {"sin": sin, "cos": cos, "exp": exp}, kwargs)
+
     return func
 
 
 def get_func_t(str_math_func: str):
     code = compile(str_math_func, "<string>", "eval")
+
     def func(t, **kwargs):
-        kwargs['t'] = t
-        return eval(code, {'sin': sin, 'cos': cos, 'exp': exp}, kwargs)
+        kwargs["t"] = t
+        return eval(code, {"sin": sin, "cos": cos, "exp": exp}, kwargs)
+
     return func
 
 
 def get_func_xt(str_math_func: str):
     code = compile(str_math_func, "<string>", "eval")
+
     def func(x, t, **kwargs):
-        kwargs['x'] = x
-        kwargs['t'] = t
-        
-        return eval(code, {'sin': sin, 'cos': cos, 'exp': exp}, kwargs)
+        kwargs["x"] = x
+        kwargs["t"] = t
+
+        return eval(code, {"sin": sin, "cos": cos, "exp": exp}, kwargs)
+
     return func
 
 
-def count_MSE(y,y_ans):
+def count_MSE(y, y_ans):
     ans = 0
     for i in range(len(y)):
-        ans += (y[i] - y_ans[i])**2
-        
+        ans += (y[i] - y_ans[i]) ** 2
+
     return np.sqrt(ans)
+
 
 start_x = 0
 finish_x = np.pi
@@ -47,105 +53,115 @@ max_t = 1
 resolution_x = 30
 resolution_t = 10
 
-Uxst_str = 'sin(x)'
-Uans_str = 'exp(-a*t)*sin(x)'
-Utsx_str = '0'
-Utfx_str = '0'
-fi_str = '0'
+Uxst_str = "sin(x)"
+Uans_str = "exp(-a*t)*sin(x)"
+Utsx_str = "0"
+Utfx_str = "0"
+fi_str = "0"
 
 
-
-
-eq = PDE.PartialDifferentialEquation(Ux_start_t=get_func_x(Uxst_str),
-                                     U_answer=get_func_xt(Uans_str),
-                                     Ut_start_x=get_func_t(Utsx_str),
-                                     Ut_finish_x=get_func_t(Utfx_str),
-                                     fi=get_func_xt(fi_str)
-                                     )
+eq = PDE.PartialDifferentialEquation(
+    Ux_start_t=get_func_x(Uxst_str),
+    U_answer=get_func_xt(Uans_str),
+    Ut_start_x=get_func_t(Utsx_str),
+    Ut_finish_x=get_func_t(Utfx_str),
+    fi=get_func_xt(fi_str),
+)
 
 
 h = (finish_x - start_x) / (resolution_x - 1)
 tau = max_t / (resolution_t - 1)
 
-x = np.array([start_x + h*i for i in range(resolution_x)])
+x = np.array([start_x + h * i for i in range(resolution_x)])
 y_ans = [eq.U_answer(elem, 0) for elem in x]
 cur_time = 0
 
 
-expimplicit = PDE.Explicit_Implicit_method(eq, start_x, finish_x,
-                                        resolution_x, max_t, resolution_t)
-explicit = PDE.Explicit_method(eq, start_x, finish_x,
-                                        resolution_x, max_t, resolution_t)
-implicit = PDE.Implicit_method(eq, start_x, finish_x,
-                                        resolution_x, max_t, resolution_t)
+expimplicit = PDE.Explicit_Implicit_method(
+    eq, start_x, finish_x, resolution_x, max_t, resolution_t
+)
+explicit = PDE.Explicit_method(eq,
+                               start_x,
+                               finish_x,
+                               resolution_x,
+                               max_t,
+                               resolution_t)
+implicit = PDE.Implicit_method(eq, start_x,
+                               finish_x,
+                               resolution_x,
+                               max_t,
+                               resolution_t)
 
 fig = plt.figure()
 fig.subplots_adjust(right=0.7, bottom=0.2, top=0.95, left=0.05)
 ax = fig.add_subplot()
 ax.grid()
-real, = ax.plot(x, y_ans, label='Аналитическое решение')
-my_plot_exp_imp, = ax.plot(x, expimplicit[0], label='Кранк-Николсона')
-my_plot_exp, = ax.plot(x, explicit[0], label='Явная схема')
-my_plot_imp, = ax.plot(x, implicit[0], label='Неявная схема')
+(real,) = ax.plot(x, y_ans, label="Аналитическое решение")
+(my_plot_exp_imp,) = ax.plot(x, expimplicit[0], label="Кранк-Николсона")
+(my_plot_exp,) = ax.plot(x, explicit[0], label="Явная схема")
+(my_plot_imp,) = ax.plot(x, implicit[0], label="Неявная схема")
 
 
+fig.text(0.84, 0.4, "Errors:")
+fig.text(0.72, 0.35, "Explicit method:")
+er_exp = fig.text(0.72, 0.32, str(count_MSE(explicit[0], y_ans)))
+fig.text(0.72, 0.29, "Implicit method:")
+er_imp = fig.text(0.72, 0.26, str(count_MSE(implicit[0], y_ans)))
 
-fig.text(0.84,0.4, 'Errors:')
-fig.text(0.72,0.35, 'Explicit method:')
-er_exp = fig.text(0.72,0.32, str(count_MSE(explicit[0], y_ans)))
-fig.text(0.72,0.29, 'Implicit method:')
-er_imp = fig.text(0.72,0.26, str(count_MSE(implicit[0], y_ans)))
+fig.text(0.72, 0.23, "Crank-Nicolson method:")
+er_exp_imp = fig.text(0.72, 0.2, str(count_MSE(expimplicit[0], y_ans)))
 
-fig.text(0.72,0.23, 'Crank-Nicolson method:')
-er_exp_imp = fig.text(0.72,0.2, str(count_MSE(expimplicit[0], y_ans)))
 
 def update_function(param_name, param_val):
-    global eq, x, cur_time, finish_x, max_t, resolution_x, resolution_t, tau, h,er_exp, er_imp, er_exp_imp
-    if param_name == 'a':
+    global eq, x, cur_time, finish_x, max_t, resolution_x, resolution_t, tau, h, er_exp, er_imp, er_exp_imp
+    if param_name == "a":
         eq.a = param_val
-    elif param_name == 'b':
+    elif param_name == "b":
         eq.b = param_val
-    elif param_name == 'c':
+    elif param_name == "c":
         eq.c = param_val
-    elif param_name == 'slider':
-        cur_time = max_t*param_val
-    elif param_name == 'fi':
+    elif param_name == "slider":
+        cur_time = max_t * param_val
+    elif param_name == "fi":
         fi_str = param_val
         eq._f_fi = get_func_xt(fi_str)
-    elif param_name == 'uxst':
+    elif param_name == "uxst":
         Uxst_str = param_val
         eq._f_Ux_start_t = get_func_x(Uxst_str)
-    elif param_name == 'utsx':
+    elif param_name == "utsx":
         Utsx_str = param_val
         eq._f_Ut_start_x = get_func_t(Utsx_str)
-    elif param_name == 'utfx':
+    elif param_name == "utfx":
         Utfx_str = param_val
         eq._f_Ut_finish_x = get_func_t(Utfx_str)
-    elif param_name == 'uans':
+    elif param_name == "uans":
         Uans_str = param_val
         eq._f_U_answer = get_func_xt(Uans_str)
-    elif param_name == 'max_x':
+    elif param_name == "max_x":
         finish_x = eval(param_val)
-    elif param_name == 'max_t':
+    elif param_name == "max_t":
         max_t = param_val
 
-    elif param_name == 'res_x':
+    elif param_name == "res_x":
         resolution_x = param_val
-    elif param_name == 'res_t':
+    elif param_name == "res_t":
         resolution_t = param_val
-        
+
     h = (finish_x - start_x) / (resolution_x - 1)
     tau = max_t / (resolution_t - 1)
-    x = np.array([start_x + h*i for i in range(resolution_x)])
+    x = np.array([start_x + h * i for i in range(resolution_x)])
     y_ans = [eq.U_answer(elem, int(cur_time / tau) * tau) for elem in x]
-    
-    expimplicit = PDE.Explicit_Implicit_method(eq, start_x, finish_x,
-                                            resolution_x, max_t, resolution_t)
-    explicit = PDE.Explicit_method(eq, start_x, finish_x,
-                                        resolution_x, max_t, resolution_t)
-    implicit = PDE.Implicit_method(eq, start_x, finish_x,
-                                        resolution_x, max_t, resolution_t)
-    
+
+    expimplicit = PDE.Explicit_Implicit_method(
+        eq, start_x, finish_x, resolution_x, max_t, resolution_t
+    )
+    explicit = PDE.Explicit_method(
+        eq, start_x, finish_x, resolution_x, max_t, resolution_t
+    )
+    implicit = PDE.Implicit_method(
+        eq, start_x, finish_x, resolution_x, max_t, resolution_t
+    )
+
     ans_exp_imp = expimplicit[int(cur_time / tau)]
     ans_exp = explicit[int(cur_time / tau)]
     ans_imp = implicit[int(cur_time / tau)]
@@ -157,19 +173,19 @@ def update_function(param_name, param_val):
     my_plot_exp.set_ydata(ans_exp)
     my_plot_imp.set_xdata(x)
     my_plot_imp.set_ydata(ans_imp)
-    
+
     er_exp.set_text(str(count_MSE(ans_exp, y_ans)))
     er_imp.set_text(str(count_MSE(ans_imp, y_ans)))
     er_exp_imp.set_text(str(count_MSE(ans_exp_imp, y_ans)))
-    
+
     ax.relim()
-    ax.set_ylim(0,1)
+    ax.set_ylim(0, 1)
     ax.legend()
     ax.autoscale_view()
 
 
 def submit_fn_a(value):
-    update_function('a', float(value))
+    update_function("a", float(value))
     plt.draw()
 
 
@@ -178,8 +194,9 @@ text_box_a = TextBox(axbox_a, "a ")
 text_box_a.on_submit(submit_fn_a)
 text_box_a.set_val(eq.a)
 
+
 def submit_fn_b(value):
-    update_function('b', float(value))
+    update_function("b", float(value))
     plt.draw()
 
 
@@ -188,10 +205,11 @@ text_box_b = TextBox(axbox_b, "b ")
 text_box_b.on_submit(submit_fn_b)
 text_box_b.set_val(eq.b)
 
+
 def submit_fn_c(value):
-    update_function('c', float(value))
+    update_function("c", float(value))
     plt.draw()
-    
+
 
 axbox_c = fig.add_axes([0.93, 0.93, 0.05, 0.05])
 text_box_c = TextBox(axbox_c, "c ")
@@ -200,18 +218,18 @@ text_box_c.set_val(eq.c)
 
 
 def submit_fn_max_x(value):
-    update_function('max_x', value)
+    update_function("max_x", value)
     plt.draw()
 
 
 axbox_max_x = fig.add_axes([0.77, 0.51, 0.08, 0.05])
 text_box_max_x = TextBox(axbox_max_x, "max_x")
 text_box_max_x.on_submit(submit_fn_max_x)
-text_box_max_x.set_val('np.pi')
+text_box_max_x.set_val("np.pi")
 
 
 def submit_fn_max_t(value):
-    update_function('max_t', float(value))
+    update_function("max_t", float(value))
     plt.draw()
 
 
@@ -220,8 +238,9 @@ text_box_max_t = TextBox(axbox_max_t, "max_t")
 text_box_max_t.on_submit(submit_fn_max_t)
 text_box_max_t.set_val(1)
 
+
 def submit_fn_res_x(value):
-    update_function('res_x', int(value))
+    update_function("res_x", int(value))
     plt.draw()
 
 
@@ -232,7 +251,7 @@ text_box_res_x.set_val(resolution_x)
 
 
 def submit_fn_res_t(value):
-    update_function('res_t', int(value))
+    update_function("res_t", int(value))
     plt.draw()
 
 
@@ -243,7 +262,7 @@ text_box_res_t.set_val(resolution_t)
 
 
 def submit_fn_fi(value):
-    update_function('fi', value)
+    update_function("fi", value)
     plt.draw()
 
 
@@ -252,8 +271,9 @@ text_box_fi = TextBox(axbox_fi, "f(x,t) ")
 text_box_fi.on_submit(submit_fn_fi)
 text_box_fi.set_val(fi_str)
 
+
 def submit_fn_uxst(value):
-    update_function('uxst', value)
+    update_function("uxst", value)
     plt.draw()
 
 
@@ -262,8 +282,9 @@ text_box_uxst = TextBox(axbox_uxst, "U(x,0)")
 text_box_uxst.on_submit(submit_fn_uxst)
 text_box_uxst.set_val(Uxst_str)
 
+
 def submit_fn_utsx(value):
-    update_function('utsx', value)
+    update_function("utsx", value)
     plt.draw()
 
 
@@ -272,8 +293,9 @@ text_box_utsx = TextBox(axbox_utsx, "U(0,t)")
 text_box_utsx.on_submit(submit_fn_utsx)
 text_box_utsx.set_val(Utsx_str)
 
+
 def submit_fn_utfx(value):
-    update_function('utfx', value)
+    update_function("utfx", value)
     plt.draw()
 
 
@@ -282,8 +304,9 @@ text_box_utfx = TextBox(axbox_utfx, "U(l,t)")
 text_box_utfx.on_submit(submit_fn_utfx)
 text_box_utfx.set_val(Utfx_str)
 
+
 def submit_fn_ans(value):
-    update_function('uans', value)
+    update_function("uans", value)
     plt.draw()
 
 
@@ -294,22 +317,21 @@ text_box_ans.set_val(Uans_str)
 
 
 def submit_fn_slider(value):
-    update_function('slider', float(value))
+    update_function("slider", float(value))
     plt.draw()
+
 
 axbox_time = fig.add_axes([0.1, 0.05, 0.8, 0.05])
 time_slider = Slider(
     ax=axbox_time,
-    label='time',
+    label="time",
     valmin=0,
     valmax=1,
     valinit=0,
 )
 
 
-
 time_slider.on_changed(submit_fn_slider)
 
 
 plt.show()
-
